4e8fea1060bfc88f7cfa630713b3d6a1b19b1cfb
goncharov <dimagoncharov21@gmail.com>
2025-03-14 17:07:17 +0300

    Подключение к БД, выполнена первая миграция создания таблицы hotels

---
+++ src/config.py
@@ -0,0 +1,24 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import  Path
+
+
+class Settings(BaseSettings):
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+
+    @property
+    def DB_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    # model_config = SettingsConfigDict(env_file=f"{Path(__file__).parent.parent / '.env'}")
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(__file__), "..", ".env"), extra="ignore"
+    )
+
+
+settings = Settings()
