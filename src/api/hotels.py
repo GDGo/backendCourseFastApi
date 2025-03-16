17c56cb054ac8ac1cdf7dddaac55e086590888c8
goncharov <dimagoncharov21@gmail.com>
2025-03-16 22:59:13 +0300

    add 'get_all' method for HotelsRepository and use it in GET /hotels

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -19,23 +19,14 @@ async def get_hotels(
         location: str | None = Query(None, description="Адрес"),
         title: str | None = Query(None, description="Название отеля")
 ):
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
-        return await HotelsRepository(session).get_all()
-    # per_page = pagination.per_page or 5
-    # async with async_session_maker() as session:
-    #     query = select(HotelsOrm)
-    #     if location:
-    #         query = query.filter(HotelsOrm.location.ilike(f"%{location.strip()}%"))
-    #     if title:
-    #         query = query.filter(HotelsOrm.title.ilike(f"%{title.strip()}%"))
-    #     query = (
-    #         query
-    #         .limit(per_page)
-    #         .offset(per_page * (pagination.page - 1))
-    #     )
-    #     result = await session.execute(query)
-    #     hotels = result.scalars().all()
-    #     return hotels
+        return await HotelsRepository(session).get_all(
+            location=location,
+            title=title,
+            limit=pagination.per_page or 5,
+            offset=per_page * (pagination.page - 1)
+        )
 
 
 #Параметр пути
--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,6 +1,27 @@
 from src.models.hotels import HotelsOrm
 from src.repositories.base import BaseRepository
+from sqlalchemy import select
 
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
+
+    async def get_all(
+            self,
+            location,
+            title,
+            limit,
+            offset
+    ):
+        query = select(HotelsOrm)
+        if location:
+            query = query.filter(HotelsOrm.location.ilike(f"%{location.strip()}%"))
+        if title:
+            query = query.filter(HotelsOrm.title.ilike(f"%{title.strip()}%"))
+        query = (
+            query
+            .limit(limit)
+            .offset(offset)
+        )
+        result = await self.session.execute(query)
+        return result.scalars().all()
