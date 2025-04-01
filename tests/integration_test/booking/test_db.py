33af9f94c6ae70f2ad0e0cf959e45abdb75630b0
GDGo <50753433+GDGo@users.noreply.github.com>
2025-04-01 22:10:02 +0300

    refactoring crud test in test_booking_crud

--- tests/integration_test/booking/test_db.py
+++ tests/integration_test/booking/test_db.py
@@ -1,6 +1,5 @@
 import time
 from datetime import date
-from enum import nonmember
 
 from src.schemas.bookings import BookingAdd
 
@@ -15,59 +14,38 @@ async def test_booking_crud(db):
         date_to=date(year=2025, month=8, day=27),
         price=100
     )
-    await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
 
-    booking = await db.bookings.get_one_or_none(
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+
+    updated_date = date(year=2025, month=8, day=30)
+    update_booking_data = BookingAdd(
         user_id=user_id,
         room_id=room_id,
         date_from=date(year=2025, month=8, day=19),
-        date_to=date(year=2025, month=8, day=27),
+        date_to=updated_date,
         price=100
-    )
-
-    assert booking
-
-    booking_data_new = BookingAdd(
-        user_id=user_id,
-        room_id=2,
-        date_from=date(year=2025, month=9, day=19),
-        date_to=date(year=2025, month=10, day=27),
-        price=200
     )
 
     await db.bookings.edit(
-        booking_data_new,
-        user_id=user_id,
-        room_id=room_id,
-        date_from=date(year=2025, month=8, day=19),
-        date_to=date(year=2025, month=8, day=27),
-        price=100
+        update_booking_data,
+        id=new_booking.id
     )
 
-    booking = await db.bookings.get_one_or_none(
-        user_id=user_id,
-        room_id=2,
-        date_from=date(year=2025, month=9, day=19),
-        date_to=date(year=2025, month=10, day=27),
-        price=200
-    )
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
 
-    assert booking
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.date_to == updated_date
 
     await db.bookings.delete(
-        user_id=user_id,
-        room_id=2,
-        date_from=date(year=2025, month=9, day=19),
-        date_to=date(year=2025, month=10, day=27),
-        price=200
+        id=new_booking.id
     )
 
-    booking = await db.bookings.get_one_or_none(
-        user_id=user_id,
-        room_id=2,
-        date_from=date(year=2025, month=9, day=19),
-        date_to=date(year=2025, month=10, day=27),
-        price=200
-    )
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
 
     assert booking is None
