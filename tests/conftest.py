adcac7716334d0d5bf3979363294acefd545c26e
GDGo <50753433+GDGo@users.noreply.github.com>
2025-04-01 16:35:46 +0300

    refactor db and ac context managers

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,41 +13,48 @@ from src.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def check_mode():
+async def check_test_mode():
     assert settings.MODE == "TEST"
 
 
+@pytest.fixture()
+async def db() -> DBManager:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        yield db
+
+
 @pytest.fixture(scope="session", autouse=True)
-async def setup_database(check_mode):
+async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
+    with open(r"tests\mock_hotels.json", encoding="utf-8") as mock_hotels, \
+            open(r"tests\mock_rooms.json", encoding="utf-8") as mock_rooms:
+        hotels = json.load(mock_hotels)
+        rooms = json.load(mock_rooms)
+
+    hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
+    rooms = [RoomAdd.model_validate(room) for room in rooms]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db_:
+        await db_.hotels.add_bulk(hotels)
+        await db_.rooms.add_bulk(rooms)
+        await db_.commit()
+
+
+@pytest.fixture(scope="session")
+async def ac() -> AsyncClient:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        yield ac
+
 
 @pytest.fixture(scope="session", autouse=True)
-async def add_data(setup_database):
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        with open(r"tests\mock_hotels.json", encoding="utf-8") as mock_hotels, \
-                open(r"tests\mock_rooms.json", encoding="utf-8") as mock_rooms:
-            hotels = json.load(mock_hotels)
-            rooms = json.load(mock_rooms)
-
-        hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
-        rooms = [RoomAdd.model_validate(room) for room in rooms]
-
-        async with DBManager(session_factory=async_session_maker_null_pool) as db:
-            await db.hotels.add_bulk(hotels)
-            await db.rooms.add_bulk(rooms)
-            await db.commit()
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def create_user(add_data):
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        await ac.post(
-            "/auth/register",
-            json={
-                "email": "kot@pes.ru",
-                "password": "1234"
-            }
-        )
+async def register_user(ac, setup_database):
+    await ac.post(
+        "/auth/register",
+        json={
+            "email": "kot@pes.ru",
+            "password": "1234"
+        }
+    )
