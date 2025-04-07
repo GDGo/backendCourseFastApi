9b07d9c8a68a1c8a4d3fe373c243e7757d8c7de9
GDGo <50753433+GDGo@users.noreply.github.com>
2025-04-07 22:57:32 +0300

    refactoring test facilities

--- tests/integration_test/facilities/test_api.py
+++ tests/integration_test/facilities/test_api.py
@@ -4,14 +4,20 @@ async def test_get_facilities(ac, db, jwt_token):
         headers={"Authorization": jwt_token},
     )
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
 
 async def test_add_facilities(ac, db, jwt_token):
+    facility_title = "Балкон"
     response = await ac.post(
         "/facilities",
         headers={"Authorization": jwt_token},
         json={
-            "title": "Балкон"
+            "title": facility_title
         }
     )
     assert response.status_code == 200
+    res = response.json()
+    assert isinstance(res, dict)
+    assert res["data"]["title"] == facility_title
+    assert "data" in res
