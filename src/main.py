19d6ac9121795449017afa59119ace17eb449c58
GDGo <50753433+GDGo@users.noreply.github.com>
2025-04-27 00:11:16 +0300

    добавил Dockerfile, .dockerignore, изменил хост на 0.0.0.0

--- src/main.py
+++ src/main.py
@@ -39,4 +39,4 @@ app.include_router(router_images)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, port=8001)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8001)
